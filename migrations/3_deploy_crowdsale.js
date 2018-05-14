b60f876a31d4f4ec709d6c271ab09f6e4585a3ec
tokenville <dev@tokenville.io>
2018-05-14 12:50:31 +0300

    fixes

--- migrations/3_deploy_crowdsale.js
+++ migrations/3_deploy_crowdsale.js
@@ -1,9 +1,11 @@
 'use strict';
 
+const RATE = "2000000000000000";
+
 const TVCrowdsale = artifacts.require("./TVCrowdsale.sol");
 const TVToken = artifacts.require("./TVToken.sol");
 
 module.exports = function(deployer, network, accounts) {
   let wallet = accounts[0];
-  return deployer.deploy(TVCrowdsale, "2000000000000000", "0x20AC118ef91569f33Ba81D4AA6ea8f0E0f855a21", TVToken.address, wallet);
+  return deployer.deploy(TVCrowdsale, RATE, wallet, TVToken.address, wallet);
 };
