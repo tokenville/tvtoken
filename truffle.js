532901ee158cc48865774df83d0ef6953e46e93e
tokenville <dev@tokenville.io>
2018-07-02 18:28:22 +0300

    mainnet ref crowdsale

--- truffle.js
+++ truffle.js
@@ -18,7 +18,7 @@ module.exports = {
     },
     mainnet: {
       provider() {
-        return new PrivateKeyProvider(process.env.PRIVATE_KEY, "https://mainnet.infura.io/")
+        return new PrivateKeyProvider(process.env.PRIVATE_KEY, "https://api.myetherapi.com/eth")
       },
       gas: 2700000,
       gasPrice: 15000000000,
