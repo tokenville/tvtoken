8ee3b9a5095a49889cf96c009f0334b46707fb95
tokenville <dev@tokenville.io>
2018-08-17 16:35:40 +0300

    revert

---
+++ tvMigration/tvRevertFunds.js
@@ -0,0 +1,519 @@
+let gasPrice = window.web3.toWei(5, 'gwei');
+
+window.onload = () => {
+  let owner = '0xb8579b19da2108249d4391d73430abba665515ca';
+
+  let holders = ["0xfc466835fb6cb2e74104be497bea840921ed3931", "0xcab5068f0cc1278d29233d2175682082c3559f4e", "0xdb6312d70961cfbb964efcd335227a68b474f638", "0xccef1314ac229a0a3680dc4f02ca7ba8838140bc", "0x63a9dbce75413036b2b778e670aabd4493aaf9f3", "0xf8e2867d1ac32f02f2e42ee0f1602e34d6bc3187", "0x346902128188c7ab7231e2fde72903333c1c3e1d", "0xc6ee9e44fc5b2f2fc0627734ea06b4df46ca221e", "0xb2815298bb6f8cec3b1faacf81b4f1ad0b111918", "0xb3589bbd2781aad79cc2727fd5abb762b522a7a2", "0x5ea1d56d0dde1ca5b50c277275855f69edefa169", "0xd75bb1e6ef53d0c65bcf53ecb19d8c64a7026c58", "0xf8215ea315efeb0b794659d5dca8b47b576cb301", "0x9b2d65c2c76a540de20bc2fba02f889e17ac346e", "0xb0d91c5d8e82a63d23b4a225ecd6249b837af891", "0xc74dcb6c536c9021e3e94467fada61f41a712062", "0xa096b47ebf7727d01ff4f09c34fc6591f2c375f0", "0xa6bcec585f12cefba9709a080ce2efd38f871024", "0xb13a1dc6fce9182bf5d401c73f285a9c2762337f", "0x721931508df2764fd4f70c53da646cb8aed16ace", "0x442dccee68425828c106a3662014b4f131e3bd9b", "0x19e2afd9a41a9122dcdebed868a89dc72718a57f", "0xddd02ef0062a8a24b9b8a327df2add8fdc7d556d", "0x2edfbd1bd4bcab2a1fd24efd4724d1344c82bb36", "0x70ffee371facaa0982b42155c9f5a1e72d453e4c", "0x22212364463f036921d34f91fdbc1f9f26ccfe00", "0x05e474a9d109c95aa8e9b48c90bde496beea17fa", "0x5c205a6ade78ed7f42f7741b89f785f989f57917"];
+  let tvOld = {
+    address: '0xF639043D58b53b1236B47FeF37dA768D2dde8507',
+    abi: [
+      {
+        "constant": true,
+        "inputs": [],
+        "name": "mintingFinished",
+        "outputs": [
+          {
+            "name": "",
+            "type": "bool"
+          }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+      },
+      {
+        "constant": true,
+        "inputs": [],
+        "name": "name",
+        "outputs": [
+          {
+            "name": "",
+            "type": "string"
+          }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+      },
+      {
+        "constant": false,
+        "inputs": [
+          {
+            "name": "_spender",
+            "type": "address"
+          },
+          {
+            "name": "_value",
+            "type": "uint256"
+          }
+        ],
+        "name": "approve",
+        "outputs": [
+          {
+            "name": "",
+            "type": "bool"
+          }
+        ],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "function"
+      },
+      {
+        "constant": true,
+        "inputs": [],
+        "name": "totalSupply",
+        "outputs": [
+          {
+            "name": "",
+            "type": "uint256"
+          }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+      },
+      {
+        "constant": false,
+        "inputs": [
+          {
+            "name": "_from",
+            "type": "address"
+          },
+          {
+            "name": "_to",
+            "type": "address"
+          },
+          {
+            "name": "_value",
+            "type": "uint256"
+          }
+        ],
+        "name": "transferFrom",
+        "outputs": [
+          {
+            "name": "",
+            "type": "bool"
+          }
+        ],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "function"
+      },
+      {
+        "constant": true,
+        "inputs": [],
+        "name": "decimals",
+        "outputs": [
+          {
+            "name": "",
+            "type": "uint8"
+          }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+      },
+      {
+        "constant": false,
+        "inputs": [],
+        "name": "unpause",
+        "outputs": [],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "function"
+      },
+      {
+        "constant": false,
+        "inputs": [
+          {
+            "name": "_to",
+            "type": "address"
+          },
+          {
+            "name": "_amount",
+            "type": "uint256"
+          }
+        ],
+        "name": "mint",
+        "outputs": [
+          {
+            "name": "",
+            "type": "bool"
+          }
+        ],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "function"
+      },
+      {
+        "constant": true,
+        "inputs": [],
+        "name": "paused",
+        "outputs": [
+          {
+            "name": "",
+            "type": "bool"
+          }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+      },
+      {
+        "constant": false,
+        "inputs": [
+          {
+            "name": "_spender",
+            "type": "address"
+          },
+          {
+            "name": "_subtractedValue",
+            "type": "uint256"
+          }
+        ],
+        "name": "decreaseApproval",
+        "outputs": [
+          {
+            "name": "success",
+            "type": "bool"
+          }
+        ],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "function"
+      },
+      {
+        "constant": true,
+        "inputs": [
+          {
+            "name": "_owner",
+            "type": "address"
+          }
+        ],
+        "name": "balanceOf",
+        "outputs": [
+          {
+            "name": "",
+            "type": "uint256"
+          }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+      },
+      {
+        "constant": false,
+        "inputs": [],
+        "name": "finishMinting",
+        "outputs": [
+          {
+            "name": "",
+            "type": "bool"
+          }
+        ],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "function"
+      },
+      {
+        "constant": false,
+        "inputs": [],
+        "name": "pause",
+        "outputs": [],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "function"
+      },
+      {
+        "constant": true,
+        "inputs": [],
+        "name": "owner",
+        "outputs": [
+          {
+            "name": "",
+            "type": "address"
+          }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+      },
+      {
+        "constant": true,
+        "inputs": [],
+        "name": "symbol",
+        "outputs": [
+          {
+            "name": "",
+            "type": "string"
+          }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+      },
+      {
+        "constant": false,
+        "inputs": [
+          {
+            "name": "_to",
+            "type": "address"
+          },
+          {
+            "name": "_value",
+            "type": "uint256"
+          }
+        ],
+        "name": "transfer",
+        "outputs": [
+          {
+            "name": "",
+            "type": "bool"
+          }
+        ],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "function"
+      },
+      {
+        "constant": false,
+        "inputs": [
+          {
+            "name": "_spender",
+            "type": "address"
+          },
+          {
+            "name": "_addedValue",
+            "type": "uint256"
+          }
+        ],
+        "name": "increaseApproval",
+        "outputs": [
+          {
+            "name": "success",
+            "type": "bool"
+          }
+        ],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "function"
+      },
+      {
+        "constant": true,
+        "inputs": [
+          {
+            "name": "_owner",
+            "type": "address"
+          },
+          {
+            "name": "_spender",
+            "type": "address"
+          }
+        ],
+        "name": "allowance",
+        "outputs": [
+          {
+            "name": "",
+            "type": "uint256"
+          }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+      },
+      {
+        "constant": false,
+        "inputs": [
+          {
+            "name": "newOwner",
+            "type": "address"
+          }
+        ],
+        "name": "transferOwnership",
+        "outputs": [],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "function"
+      },
+      {
+        "inputs": [],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "constructor"
+      },
+      {
+        "anonymous": false,
+        "inputs": [
+          {
+            "indexed": true,
+            "name": "to",
+            "type": "address"
+          },
+          {
+            "indexed": false,
+            "name": "amount",
+            "type": "uint256"
+          }
+        ],
+        "name": "Mint",
+        "type": "event"
+      },
+      {
+        "anonymous": false,
+        "inputs": [],
+        "name": "MintFinished",
+        "type": "event"
+      },
+      {
+        "anonymous": false,
+        "inputs": [],
+        "name": "Pause",
+        "type": "event"
+      },
+      {
+        "anonymous": false,
+        "inputs": [],
+        "name": "Unpause",
+        "type": "event"
+      },
+      {
+        "anonymous": false,
+        "inputs": [
+          {
+            "indexed": true,
+            "name": "previousOwner",
+            "type": "address"
+          },
+          {
+            "indexed": true,
+            "name": "newOwner",
+            "type": "address"
+          }
+        ],
+        "name": "OwnershipTransferred",
+        "type": "event"
+      },
+      {
+        "anonymous": false,
+        "inputs": [
+          {
+            "indexed": true,
+            "name": "owner",
+            "type": "address"
+          },
+          {
+            "indexed": true,
+            "name": "spender",
+            "type": "address"
+          },
+          {
+            "indexed": false,
+            "name": "value",
+            "type": "uint256"
+          }
+        ],
+        "name": "Approval",
+        "type": "event"
+      },
+      {
+        "anonymous": false,
+        "inputs": [
+          {
+            "indexed": true,
+            "name": "from",
+            "type": "address"
+          },
+          {
+            "indexed": true,
+            "name": "to",
+            "type": "address"
+          },
+          {
+            "indexed": false,
+            "name": "value",
+            "type": "uint256"
+          }
+        ],
+        "name": "Transfer",
+        "type": "event"
+      },
+      {
+        "constant": false,
+        "inputs": [
+          {
+            "name": "_from",
+            "type": "address"
+          },
+          {
+            "name": "_to",
+            "type": "address"
+          },
+          {
+            "name": "_value",
+            "type": "uint256"
+          }
+        ],
+        "name": "revertFunds",
+        "outputs": [
+          {
+            "name": "",
+            "type": "bool"
+          }
+        ],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "function"
+      }
+    ],
+    contract: null
+  };
+  let web3 = window.web3;
+
+  tvOld.contract = web3.eth.contract(tvOld.abi).at(tvOld.address);
+
+  document.getElementById('revert').addEventListener('click', () =>
+    holders.map(holder => balanceOf(tvOld.contract, holder)
+      .then(balance => {
+        if (+balance === 0) {
+          console.error('Tokens already reverted ', holder)
+        } else {
+          revert(tvOld.contract, owner, holder, balance)
+        }
+      })
+    )
+  );
+
+  document.getElementById('check').addEventListener('click', () => {
+    holders.map(holder => balanceOf(tvOld.contract, holder)
+      .then(balance => {
+        if (+balance > 0) {
+          console.error('Holder have TVs: ', holder, web3.fromWei(balance, 'ether').toString())
+        } else {
+          console.log('Correct ', holder)
+        }
+      })
+    )
+  })
+};
+
+function balanceOf(contract, account) {
+  // console.log('Invoke balanceOf ', account);
+  return new Promise(res =>
+    contract.balanceOf(account, (e, data) => {
+      // console.log("Result balanceOf.", "Error: " + e, "Data: " + window.web3.fromWei(data, 'ether'));
+      e && console.error(e, account);
+      data && res(data);
+    })
+  )
+}
+
+
+function revert(contract, from, to, amount) {
+  console.log('Invoke revertFunds.', from, to, amount);
+  return new Promise(res =>
+    contract.revertFunds(from, to, amount, {from, gasPrice}, (e, data) => {
+      e && console.error(e, to);
+      data && res(data);
+    })
+  )
+}
