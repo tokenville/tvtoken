f089fa448fab178287f79b5e9a271ba8effb5a09
Nike <dev@tokenville.io>
2018-12-20 17:29:55 +0300

    redeploy crowdsale

--- contracts/TVCrowdsale.sol
+++ contracts/TVCrowdsale.sol
@@ -6,20 +6,43 @@ import 'zeppelin-solidity/contracts/crowdsale/Crowdsale.sol';
 
 contract TVCrowdsale is AllowanceCrowdsale, Ownable {
   uint256 public currentRate;
+  address public manager;
+
+  modifier onlyOwnerOrManager() {
+    require(msg.sender == owner || manager == msg.sender);
+    _;
+  }
   
-  function TVCrowdsale(uint256 _rate, address _wallet, ERC20 _token, address _tokenWallet)
+  constructor(uint256 _rate, address _wallet, ERC20 _token, address _tokenWallet, address _manager)
     Crowdsale(_rate, _wallet, _token)
     AllowanceCrowdsale(_tokenWallet) public {
     currentRate = _rate;
+    manager = _manager;
   }
   
-  function setRate(uint256 _rate) public onlyOwner returns (bool) {
+  function setRate(uint256 _rate) public onlyOwnerOrManager returns (bool) {
     currentRate = _rate;
     return true;
   }
-  
+
+  function changeTokenWallet(address _tokenWallet) public onlyOwnerOrManager {
+    _tokenWallet = tokenWallet;
+  }
+
+  function changeWallet(address _wallet) public onlyOwnerOrManager {
+    wallet = _wallet;
+  }
+
+  function changeToken(ERC20 _token) public onlyOwnerOrManager {
+    token = _token;
+  }
+
   function _getTokenAmount(uint256 _weiAmount) internal view returns (uint256) {
     return _weiAmount.mul(currentRate);
+  }
+
+  function setManager(address _manager) public onlyOwner {
+    manager = _manager;
   }
 
 }
--- migrations/3_deploy_crowdsale.js
+++ migrations/3_deploy_crowdsale.js
@@ -1,11 +1,14 @@
 'use strict';
 
-const RATE = "500";
+const RATE = "200";
 
 const TVCrowdsale = artifacts.require("./TVCrowdsale.sol");
 const TVToken = artifacts.require("./TVToken.sol");
 
+const wallet = '0x120ceddf37ed9704f8f3f226d4d85caa4ef20b63';
+const token_wallet = '0x09570e69ADDD835a71bd19a5104656e0Da38fD84';
+const manager = '0x1feD8Ba9A9FDd72EF9038046ad148bEb413491b8';
+
 module.exports = function(deployer, network, accounts) {
-  let wallet = accounts[0];
-  return deployer.deploy(TVCrowdsale, RATE, process.env.WALLET || wallet, TVToken.address, wallet);
+  return deployer.deploy(TVCrowdsale, RATE, wallet, TVToken.address, token_wallet, manager);
 };
